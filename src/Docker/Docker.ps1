b7e5845e29152e5d324ecb2c64379099c0f0ac24
MoRevolution <81017119+MoRevolution@users.noreply.github.com>
2025-05-28 09:49:54 -0500

    trying to simplify this thing has way to many features, gotta trim it down to just windows setup and wsl2 setup

--- src/Docker/Docker.ps1
+++
@@ -1,1 +0,0 @@
-choco install -y "docker-desktop" --execution-timeout 3600;
--- src/Helpers/External.ps1
+++
@@ -1,2 +0,0 @@
-# PS-SFTA: Library to associate applications with their file extensions.
-Invoke-Expression (Invoke-RestMethod -Uri "https://raw.githubusercontent.com/DanysysTeam/PS-SFTA/master/SFTA.ps1");
