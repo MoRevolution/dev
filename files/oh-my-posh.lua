40aaf0033f054bbe198b5b559f6b55b7bc44f119
unknown <MoRevolution@WDX2MQ50906WW0.jdnet.deere.com>
2025-09-17 07:00:32 -0500

    feat: added new install script, python + .toml
    much much simpler

--- files/oh-my-posh.lua
+++ files/oh-my-posh.lua
@@ -1,1 +1,2 @@
-"load(io.popen('oh-my-posh init cmd'):read("*a"))()" 
+local home = os.getenv("USERPROFILE") -- Use "HOME" on Unix-like systems
+load(io.popen('oh-my-posh init cmd --config ' .. home .. '\\zsh-ish.omp.json'):read("*a"))()
